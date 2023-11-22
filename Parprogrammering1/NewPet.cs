7159b31d3ff85220da7a324dc7b3f090ad6e8ab4
ListrikLair <kevin.strom.trondsetas@gmail.com>
2023-11-22 13:38:29 +0100

    Add project files.

---
+++ Parprogrammering1/NewPet.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parprogrammering1
+{
+    internal class NewPet
+    {
+        public string Name;
+        public int Age;
+        public bool NeedFood = true;
+        public bool NeedPoop;
+        public bool NeedPets;
+
+        public NewPet(string name, int age, bool needFood, bool needPoop, bool needPets)
+        {
+            Name = name;
+            Age = age;
+            NeedFood = needFood;
+            NeedPoop = needPoop;
+            NeedPets = needPets;
+        }
+
+        public NewPet(string name)
+        {
+            Name = name;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Your pet's name is {Name} and it's age is {Age} NeedFood = {NeedFood}");
+        }
+
+        public void IsHungry()
+        {
+            
+        }
+    }
+}
---
+++ Parprogrammering1/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Parprogrammering1
+{
+    class Program
+    {
+        public static void Main(string[] arg)
+        {
+            Init();
+            var pet1 = new NewPet("Kåre");
+            pet1.Show();
+
+            
+            static void CreatePet(string input)
+            {
+                var inputName = new NewPet(input);
+                inputName.Show();
+            }
+
+            static void Init()
+            {
+                Console.WriteLine("What is the name of your new pet?");
+                var inputName1 = Console.ReadLine();
+                Console.WriteLine("What is the name of your second pet?");
+                var inputName2 = Console.ReadLine();
+                Console.WriteLine("What is the name of your third pet?");
+                var inputName3 = Console.ReadLine();
+                CreatePet(inputName);
+            }
+
+            static void Continue()
+            {
+                Console.Clear();
+                if(pet1.NeedFood){}
+            }
+        }
+    }
+}
